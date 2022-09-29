3f740de956416dc857a08dc74249825fe216c3e8
Ademola <ademolaogunmokun@yahoo.com>
2022-09-29 14:33:31 +0100

    route to sign in complete

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,8 @@ import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
-import { Route, Routes, Navigate } from 'react-router-native';
+import { Route, Routes, Navigate,} from 'react-router-native';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
 
@@ -18,8 +19,9 @@ const Main = () => {
     <View style = {styles.container} >
       <AppBar/>
       <Routes>
-        <Route path="/" element={<RepositoryList/>}/>
-        <Route path= '*' element={<Navigate to="/" />}/>
+        <Route path="/" element={<RepositoryList/>} exact/>
+        <Route path='signin' element = {<SignIn/>} exact />
+        <Route path= '*' element={<Navigate to="/"  replace/> }/>
       </Routes>
 
     </View>
