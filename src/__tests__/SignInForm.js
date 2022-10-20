f18c07291c7841c8a4cff190fa6b41049f081f86
Ademola <ademolaogunmokun@yahoo.com>
2022-10-20 15:41:33 +0100

    testing non side effect

--- src/__tests__/SignInForm.js
+++ src/__tests__/SignInForm.js
@@ -1,36 +1,59 @@
-import SignIn from '../Components/SignIn';
+
 import { render, fireEvent } from '@testing-library/react-native';
-import { NativeRouter } from 'react-router-native';
-import { ApolloProvider } from '@apollo/client';
-import AuthStorage from '../utils/authStorage';
-import createApolloClient from '../utils/apolloClient';
-import AuthStorageContext from '../contexts/AuthStorageContext';
 
 
-const authStorage = new AuthStorage();
-const apolloClient = createApolloClient(authStorage);
+import { useState } from 'react';
+import { Text, TextInput, Pressable, View } from 'react-native';
 
 
-describe('SignInForm', () => {
-  it('calls function provided by the onSubmit props after pressing the button', () => {
+const Form = ({ onSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    onSubmit({ username, password });
+  };
+
+  return (
+    <View>
+      <View>
+        <TextInput
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          placeholder="Username"
+        />
+      </View>
+      <View>
+        <TextInput
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder="Password"
+        />
+      </View>
+      <View>
+        <Pressable onPress={handleSubmit}>
+          <Text>Submit</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+describe('Form', () => {
+  it('calls function provided by onSubmit prop after pressing the submit button', () => {
     const onSubmit = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <NativeRouter>
-        <ApolloProvider client={apolloClient}>
-          <AuthStorageContext.Provider value={authStorage}>
-            <SignIn onSubmit={onSubmit} />
+    const { getByPlaceholderText, getByText } = render(<Form onSubmit={onSubmit} />);
 
-          </AuthStorageContext.Provider>
-        </ApolloProvider>
-      </NativeRouter>);
-    fireEvent.changeText(getByPlaceholderText('Username'), 'Kalle');
+    fireEvent.changeText(getByPlaceholderText('Username'), 'kalle');
     fireEvent.changeText(getByPlaceholderText('Password'), 'password');
-    fireEvent.press(getByText('Sign in'));
+    fireEvent.press(getByText('Submit'));
+
     expect(onSubmit).toHaveBeenCalledTimes(1);
 
-    // fireEvent.press(getByTestId('submitButton'));
-    // expect(onSubmit).toHaveBeenCalledTimes(1);
-
+    // onSubmit.mock.calls[0][0] contains the first argument of the first call
+    expect(onSubmit.mock.calls[0][0]).toEqual({
+      username: 'kalle',
+      password: 'password',
+    });
   });
-
 });
