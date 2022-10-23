6e85cff2f56f83575294f07f54b9e34919ec8241
Ademola <ademolaogunmokun@yahoo.com>
2022-10-23 12:57:28 +0100

    finish displaying review

--- src/Components/SingleRepo.jsx
+++ src/Components/SingleRepo.jsx
@@ -1,9 +1,6 @@
 import { View,  Button, StyleSheet, Pressable } from 'react-native';
 import React from 'react';
 import RepositoryItem from './RepositoryItem';
-import { useParams } from 'react-router-native';
-import { REPOSITORY } from '../graphql/queries';
-import { useQuery } from '@apollo/client';
 import { Linking } from 'react-native';
 
 const styles = StyleSheet.create({
